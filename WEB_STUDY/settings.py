ee3ddd8c38adeb229356d83adc5787b4473d8173
root <mskim9967@gmail.com>
2021-02-06 14:53:00 +0000

    modify setting

--- WEB_STUDY/settings.py
+++ WEB_STUDY/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [".run.goorm.io"]
 
 INSTALLED_APPS = [
     'polls',
+    'bookmark',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'WEB_STUDY.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
--- WEB_STUDY/urls.py
+++ WEB_STUDY/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('bookmark/', include('bookmark.urls')),
     path('admin/', admin.site.urls),
 ]
